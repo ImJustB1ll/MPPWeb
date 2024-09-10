eb55be84afc3edf0eb719230e0557f4005b8c192
BillyJouaqin <jouaqinbilly@gmail.com>
2024-09-10 08:58:45 +0700

    Changing JS

--- js/carousel.js
+++ js/carousel.js
@@ -1,35 +1,55 @@
 const track = document.querySelector('.carousel_track');
 const slides = Array.from(track.children);
-const nextButton = document.querySelector('.carousel_button-right');
-const prevButton = document.querySelector('.carousel_button-left');
+const nextButton = document.querySelector('.carousel_button--right');
+const prevButton = document.querySelector('.carousel_button--left');
 const dotsNav = document.querySelector('.carousel_nav');
 const dots = Array.from(dotsNav.children);
 
 const slideWidth = slides[0].getBoundingClientRect().width;
 
-// Samping sampingan
-
-// slides[0].style.left = slideWidth * 0 + 'px';
-// slides[1].style.left = slideWidth * 1 + 'px';
-// slides[2].style.left = slideWidth * 2 + 'px';
-
+// Set initial slide positions
 const setSlidePosition = (slide, index) => {
-    slide.style.left = slideWidth * index + 'px';
+  slide.style.left = slideWidth * index + 'px';
 };
 slides.forEach(setSlidePosition);
 
-// Left go to Left
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+    targetDot.classList.add('current-slide');
+}
 
-
-// Right go to Right
+// Add event listener to next button
 nextButton.addEventListener('click', e => {
-    
-    const currentSlide = track.querySelector('current-slide');
-    const nextSlide = currentSlide.nextElementSibling;
-    const amountToMove = nextSlide.style.left;
-    // Move to next slide
-    track.style.transform = 'translateX(-' + amountToMove + ')';
+  const currentSlide = track.querySelector('.current-slide');
+  const nextSlide = currentSlide.nextElementSibling;
+  if (nextSlide) {
+    const amountToMove = nextSlide.offsetLeft;
+    track.style.transform = `translateX(-${amountToMove}px)`;
+    currentSlide.classList.remove('current-slide');
+    nextSlide.classList.add('current-slide');
+  }
+});
 
-})
+// Add event listener to prev button (you didn't have this implemented)
+prevButton.addEventListener('click', e => {
+  const currentSlide = track.querySelector('.current-slide');
+  const prevSlide = currentSlide.previousElementSibling;
+  if (prevSlide) {
+    const amountToMove = prevSlide.offsetLeft;
+    track.style.transform = `translateX(-${amountToMove}px)`;
+    currentSlide.classList.remove('current-slide');
+    prevSlide.classList.add('current-slide');
+  }
+});
 
-// Click, move to active
+// Add event listener to dots navigation (you didn't have this implemented)
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const targetSlide = slides[index];
+    const amountToMove = targetSlide.offsetLeft;
+    track.style.transform = `translateX(-${amountToMove}px)`;
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+  });
+});
